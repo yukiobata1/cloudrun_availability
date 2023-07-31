b1a59af047e9a9689d709d6c32e07b2e19d7604b
obatayuki <yukiobata1@gmail.com>
2023-07-31 20:05:59 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,8 +44,7 @@ def save_to_excel(df, file_name):
     ws.cell(row=total_rows, column=2, value=len(df)*4)
 
     # Save the Excel file
-    print(f'{file_name=}')
-    wb.save(file_name.split(".")[1])
+    wb.save(file_name)
 
 
 def check_schedule_within_30_minutes():
